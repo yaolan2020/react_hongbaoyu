9b9770db455cb39859570732e1f6a293883eaec5
yaolan2020 <1216827215@qq.com>
2020-05-06 16:15:52 +0800

    删除配置文件

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path=require('path');
-const server=express();
-
-server.use(express.static(path.join(__dirname,'./')));
-server.listen(3000,()=>{
-    console.log('app listening at port 3000')
-});
