5279ebb5d09368e1d79c5374749ceeece9f357f5
yaolan2020 <1216827215@qq.com>
2020-05-06 18:51:11 +0800

    build: git自动部署测试

---
+++ build/precache-manifest.dbcf2508427d4c10ddf8bdedeab3213a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0886c6d3be2d25f4dbedd4845e156141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "458d2c1d4da537e726a4",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "833d0394b5f2afc47554",
+    "url": "/static/js/2.9a117118.chunk.js"
+  },
+  {
+    "revision": "458d2c1d4da537e726a4",
+    "url": "/static/js/main.7ad5e430.chunk.js"
+  },
+  {
+    "revision": "3a8e1c2e9b75c3cd1175",
+    "url": "/static/js/runtime-main.37974ebd.js"
+  }
+]);
