bc48aa73da563c97d74529156f753d2c7e009f82
yaolan2020 <1216827215@qq.com>
2020-05-14 18:23:49 +0800

    feat: 主页添加红包雨动画

--- src/view/main/App.js
+++ src/view/main/App.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-import Config from '../../other/Config';
-
+import $ from 'jquery'
 import './App.css';
+import redPackage from './../../images/red-package.png';
 
 class App extends  React.Component {
+    constructor(props){
+      super(props);
+      this.state={
+        name:'诉诉飞飞',
+        screenwidth:window.screen.width,
+      }
+      this.snowDrop(50, this.randomInt(1035, this.state.screenwidth-60));
+      this.snow(50, 150);
+    };
+    snow=(num, speed)=> {
+		if (num > 0) {
+            let self=this;
+			setTimeout(function () {
+				$('#drop_' + self.randomInt(1, 250)).addClass('animate');
+				self.snow(num--, speed);
+			}, speed);
+		}
+    };
+    snowDrop=(num,position)=>{
+        if (num > 0) {
+            var drop = `<div class="drop snow" id=${'drop_'+num}>
+                           <img class='redPackage' src='${redPackage}'>
+                        </div>`;
+			$('body').append(drop);
+			$('#drop_' + num).css('left', position);
+			num--;
+			this.snowDrop(num, this.randomInt(60, this.state.screenwidth-60));
+		}
+    };
+    randomInt=(min,max)=>{
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
     render() {
         return (
             <div className="App">
-                计数器：<button type='button'>-</button>0<button type='button'>+</button>
+                <p className='app_name'>{this.state.name}</p>
+                 
             </div>
         );
     }
